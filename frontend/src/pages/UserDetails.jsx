978ffce46afff04e70ec238936da5df92af77f37
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2025-01-07 11:04:34 +0530

    krde

---
+++ frontend/src/pages/UserDetails.jsx
@@ -0,0 +1,16 @@
+
+
+export default function UserDetails() {
+  return (
+    <div className="bg-white max-w-screen-2xl rounded-xl p-4">
+      <div className="flex justify-between">
+        <div className="mx-3">
+            <h1 className="text-3xl font-semibold ">Rayyan Shaikh</h1>
+            <p className="text-sm">24 years | Mumbai</p>
+            <span>User</span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
