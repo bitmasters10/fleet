97864ed145cbcf44595faad04005c3cffccb907a
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2025-02-01 11:23:35 +0530

    PUSH WS ON ADMIN DONE

--- backend/db.js
+++ backend/db.js
@@ -14,6 +14,18 @@ const db = mysql.createPool({
     connectTimeout: 10000,
   });
   
+// const db = mysql.createPool({
+//   host: 'localhost',     // Replace with your host
+//    user: 'root',          // Replace with your username
+//   password: '',  // Replace with your password
+//   database: 'u820563802_Linda_fleet',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 10000,
+// });
+
+  
   db.getConnection((err, connection) => {
     if (err) {
       console.error('Error connecting to the database:', err);
