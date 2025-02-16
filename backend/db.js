6285e3642e29c8c272cb279fecf1f214024861cb
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2025-02-16 15:11:57 +0530

    nya bharat ka nya backend

--- backend/db.js
+++ backend/db.js
@@ -4,15 +4,18 @@ const mysql = require('mysql2');
 
 
 const db = mysql.createPool({
-    host: 'fleet.lindatours.in',     // Replace with your host
-     user: 'u820563802_Linda_fleet',          // Replace with your username
-    password: 'Fleet@1234',  // Replace with your password
-    database: 'u820563802_Linda_fleet',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 10000,
-  });
+  host: '103.21.59.28',
+  port: 3306,  // Ensure this is correct
+  user: 'u820563802_Linda_fleet',
+  password: 'Fleet@1234',
+  database: 'u820563802_Linda_fleet',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
+});
+
+
   
 // const db = mysql.createPool({
 //   host: 'localhost',     // Replace with your host
