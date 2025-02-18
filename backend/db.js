88ae2de57cda8076387facf50c930a48db30ef57
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2025-02-18 09:22:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,30 +3,30 @@ const mysql = require('mysql2');
 
 
 
-const db = mysql.createPool({
-  host: '103.21.59.28',
-  port: 3306,  // Ensure this is correct
-  user: 'u820563802_Linda_fleet',
-  password: 'Fleet@1234',
-  database: 'u820563802_Linda_fleet',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000,
-});
-
-
-  
 // const db = mysql.createPool({
-//   host: 'localhost',     // Replace with your host
-//    user: 'root',          // Replace with your username
-//   password: '',  // Replace with your password
+//   host: 'fleet.lindatours.in',
+//   port: 3306,  // Ensure this is correct
+//   user: 'u820563802_Linda_fleet',
+//   password: 'Fleet@1234',
 //   database: 'u820563802_Linda_fleet',
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0,
 //   connectTimeout: 10000,
 // });
+
+
+  
+const db = mysql.createPool({
+  host: 'localhost',     // Replace with your host
+   user: 'root',          // Replace with your username
+  password: '',  // Replace with your password
+  database: 'u820563802_Linda_fleet',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
+});
 
   
   db.getConnection((err, connection) => {
