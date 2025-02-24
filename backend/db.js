b0097d292ca7d29f9e71d2eb0d41938be605e297
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2025-02-24 16:20:53 +0530

    fs

--- backend/db.js
+++ backend/db.js
@@ -15,30 +15,30 @@ const mysql = require('mysql2');
 
 
     
-// const db = mysql.createPool({
-//   host: 'localhost',     // Replace with your host
-//    user: 'root',          // Replace with your username
-//   password: '',  // Replace with your password
-//   database: 'u820563802_Linda_fleet',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000,
-// });
+const db = mysql.createPool({
+  host: 'localhost',     // Replace with your host
+   user: 'root',          // Replace with your username
+  password: '',  // Replace with your password
+  database: 'u820563802_Linda_fleet',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
+});
 
 
 
   
-const db = mysql.createPool({
-    host: 'fleet.lindatours.in',     
-     user: 'u820563802_Linda_fleet',          // Replace with your username
-    password: 'Fleet@1234',  // Replace with your password
-    database: 'u820563802_Linda_fleet',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 10000,
-  });
+// const db = mysql.createPool({
+//     host: 'fleet.lindatours.in',     
+//      user: 'u820563802_Linda_fleet',          // Replace with your username
+//     password: 'Fleet@1234',  // Replace with your password
+//     database: 'u820563802_Linda_fleet',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     connectTimeout: 10000,
+//   });
 
   
   db.getConnection((err, connection) => {
