5a1c9e97883b0e45f2d54a3c8917c83cbd5c6558
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2025-03-05 18:44:09 +0530

    chnges

--- DriversApp/config.js
+++ DriversApp/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://fleet-git-master-bitmasters-projects.vercel.app'  // Make sure this matches your backend URL
+    API_URL: 'https://fleet-65ctck3ia-bitmasters-projects.vercel.app/'  // Make sure this matches your backend URL
   };
   
   export default config;
