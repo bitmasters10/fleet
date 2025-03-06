e939ea3bdc40bc16df6179a6b4ace00ce8ac28eb
CHITESHVARUN10 <109293694+CHITESHVARUN10@users.noreply.github.com>
2025-03-06 17:59:50 +0530

    256

--- DriversApp/screen/MapScreen.js
+++ DriversApp/screen/MapScreen.js
@@ -20,7 +20,7 @@ import config from "../config";
 
 const { width } = Dimensions.get("window");
 
-const socket = io("ws://192.168.10.122:3001");
+const socket = io("https://websoc-4ibu.onrender.com1");
 const HOME=config.API_URL
 const home = HOME;
 
